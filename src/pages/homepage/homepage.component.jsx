54d532fcd21f3a8345661ff2126e08237554dd1c
wil.i.am <wiliam.tae@gmail.com>
2019-12-12 09:26:45 -0300

    directory and menu-item component created

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,48 +1,11 @@
 import React from 'react';
 import './homepage.styles.scss';
-
+import Directory from '../../components/directory/directory.component.jsx';
 
 const HomePage = () => (
   <div className='homepage'>
   <h1>Bem vindo à Harajuko</h1>
-    <div className='directory-menu'>
-
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>Chapéus</h1>
-          <span className='subtitle'>Compre Agora</span>
-        </div>
-      </div>
-
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>Capas</h1>
-          <span className='subtitle'>Compre Agora</span>
-        </div>
-      </div>
-
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>Blusas</h1>
-          <span className='subtitle'>Compre Agora</span>
-        </div>
-      </div>
-
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>Saias</h1>
-          <span className='subtitle'>Compre Agora</span>
-        </div>
-      </div>
-
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>Cosplays</h1>
-          <span className='subtitle'>Compre Agora</span>
-        </div>
-      </div>
-
-    </div>
+    <Directory/>
   </div>
 );
 
