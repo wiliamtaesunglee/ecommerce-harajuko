039f7125dd29f72b687158c5dde4c49e26466d70
wil.i.am <wiliam.tae@gmail.com>
2019-12-30 18:56:19 -0300

    login firebase with email and pasword

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './sign-in-and-sign-up.styles.scss';
+
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
+import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUp = () => (
-  <div>
+  <div className='sign-in-sign-up'>
     <SignIn/>
+    <SignUp/>
   </div>
 );
 
