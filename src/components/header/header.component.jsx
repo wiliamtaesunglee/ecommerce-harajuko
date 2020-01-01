cd65628d309894cd756abf5c92eede72703a2907
wil.i.am <wiliam.tae@gmail.com>
2020-01-01 15:49:48 -0300

    cart dropdown menu

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.component.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
@@ -28,6 +29,7 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon/>
     </div>
+    <CartDropdown/>
   </div>
 );
 
