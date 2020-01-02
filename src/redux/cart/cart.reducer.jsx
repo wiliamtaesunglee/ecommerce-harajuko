bbcc49129db923cacbab3771aced4a9913f1a4f7
wil.i.am <wiliam.tae@gmail.com>
2020-01-02 16:02:36 -0300

    add item quantity feature created

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,4 +1,5 @@
 import CartActionType from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -12,10 +13,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       }
-    case CartActionType.CART_ITEM:
+    case CartActionType.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload]
+        cartItems: addItemToCart(state.cartItems, action.payload)
       }
     default: return state
   }
